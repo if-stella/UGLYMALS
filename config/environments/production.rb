332da9cdabe5ddf7198f3e8c3e5a8ab4968f9015
Stella Raab <hallo@stellaraab.de>
2022-02-21 17:16:51 +0100

    Installed devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "UGLYMALS_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://uglymals.herokuapp.com/', port: 3000 }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
