0407e4a658d0648a297f9bc6758741596f460597
Ida Schwarzkopf <ida.schwarzkopf@ha-es.de>
2022-02-22 12:27:17 +0100

    added seeds

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,6 @@
 class Animal < ApplicationRecord
   belongs_to :user
-  validates :name, :age, :species, presence: true
-  validates :age, numericality: { only_integer: true }
-  validates :name, length: { minimum: 3 }
+  # validates :name, :age, :species, presence: true
+  # validates :age, numericality: { only_integer: true }
+  # validates :name, length: { minimum: 3 }
 end
